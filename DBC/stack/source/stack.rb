b90e5879d3500d4a1911544d4c0d718b8e7e3b2d
godspeedyoo <godspeedyoo@gmail.com>
2015-03-14 20:08:47 -0700

    Implement stack data structure as class Stack

--- DBC/stack/source/stack.rb
+++ DBC/stack/source/stack.rb
@@ -1,3 +1,18 @@
 class Stack
+	def initialize
+		@store = Array.new
+	end
 
+	def push(item)
+		@store.push(item)
+		self
+	end
+
+	def pop
+		@store.pop
+	end
+
+	def size
+		@store.length
+	end
 end
