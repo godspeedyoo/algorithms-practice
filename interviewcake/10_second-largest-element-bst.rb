bffdacef15026b13884b3274506ea8c943273224
godspeedyoo <godspeedyoo@gmail.com>
2015-04-05 15:30:01 -0700

    refactor iterative solution to find second largest element in bst

--- interviewcake/10_second-largest-element-bst.rb
+++ interviewcake/10_second-largest-element-bst.rb
@@ -2,22 +2,39 @@
 
 def find_second_largest root
 	return nil unless root
-
-	current_node = root
+	return nil unless root.right || root.left
 	parent_stack = []
-	# traverse to find max int (right most)
-	until current_node.right.nil?
-		parent_stack << current_node
-		current_node = current_node.right
-	end
-	# current_node is now the max value of tree
-	# now check for a left subtree, otherwise return parent
-	current_node = current_node.left if current_node.left
 
-	# if left subtree exists, travers right to find the second largest value
-	until current_node.right.nil?
-		current_node = current_node.right
+	# check if there is a right subtree
+	if root.right
+		current_node = root
+		# traverse to find max int (right most)
+		until current_node.right.nil?
+			parent_stack << current_node
+			current_node = current_node.right
+		end
+		# current_node is now the max value of tree
+		# now check for a left subtree, otherwise return parent
+		if current_node.left
+			current_node = current_node.left
+			# if left subtree exists, travers right to find the second largest value
+			current_node = find_max current_node
+		else
+			return parent_stack.pop
+		end
+	# return second largest within left subtree if left exists and right does not
+	elsif root.left && root.right.nil?
+		current_node = root.left
+		return current_node unless current_node.right
+		return find_max current_node
 	end
 	
+end
+
+
+def find_max node
+	until node.right.nil?
+		current_node = current_node.right
+	end
 	current_node
 end
